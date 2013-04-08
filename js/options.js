2d6fa386e141a2bb394121f05cbc69615c2f07b2
Uffe Graakjaer <uffegraa@gmail.com>
2013-04-08 17:42:13 +0100

    Greeting and Shortcuts are now both options
    It is now possible to turn both greeting and shortcuts feature on/off
    via options.
    
    A table has also been added to the options page, though it isn't used
    for anything yet.  The idea is that this table is turned into an
    editable table in which users specify details about their shortcuts.

--- js/options.js
+++ js/options.js
@@ -1,14 +1,36 @@
+function insertShortcutRow() {
+   var table = document.getElementById("shortcutsTable");
+   var row = table.insertRow(-1);
+   var cell1 = row.insertCell(0);
+   var cell2 = row.insertCell(1);
+   var cell3 = row.insertCell(2);
+   var cell4 = row.insertCell(3);
+   cell1.innerHTML = "New";
+   cell2.innerHTML = "New";
+   cell3.innerHTML = "New";
+   cell4.innerHTML = "New";
+}
+
 function saveOptions() {
    var select = document.getElementById("greeting");
    var greeting = select.children[select.selectedIndex].value;
 
    chrome.storage.sync.set({'greetingOption': greeting}, function() {
-      var status = document.getElementById("status");
-      status.innerHTML = "Options Saved.";
-      setTimeout(function() {
-         status.innerHTML = "";
-      }, 750);
    });
+
+   var select = document.getElementById("shortcuts");
+   var shortcuts = select.children[select.selectedIndex].value;
+
+   chrome.storage.sync.set({'shortcutsOption': shortcuts}, function() {
+   });
+
+   var status = document.getElementById("status");
+
+   status.innerHTML = "Options Saved.";
+
+   setTimeout(function() {
+      status.innerHTML = "";
+   }, 750);
 }
 
 function restoreOptions() {
@@ -19,9 +41,22 @@ function restoreOptions() {
       }
       else {
          document.getElementById("greeting").value = greetingvalue;
-   }
+      }
+   });
+
+   chrome.storage.sync.get('shortcutsOption', function(r) {
+      var greetingvalue = r['shortcutsOption'];
+      if (!greetingvalue) {
+         document.getElementById("shortcuts").value = 'No';
+      }
+      else {
+         document.getElementById("shortcuts").value = greetingvalue;
+      }
    });
 }
 
 document.addEventListener('DOMContentLoaded', restoreOptions);
 document.getElementById("saveButton").addEventListener('click', saveOptions);
+document.addEventListener('DOMContentLoaded', function() {
+   document.getElementById("insertShortcutRowButton").addEventListener('click', insertShortcutRow);
+});
--- js/psutilities.js
+++ js/psutilities.js
@@ -1,149 +1,159 @@
    //
-   // PS GREETING BEGIN
+   // GREETING BEGIN
    //
-
    chrome.storage.sync.get('greetingOption', function(r) {
       var greetingvalue = r['greetingOption'];
 
-   if(greetingvalue=='Yes') {
+      if (greetingvalue == 'Yes') {
+         var locStr = window.location.href;
 
-   var locStr=window.location.href;
+         //Find psp in URL
+         var envBegin = locStr.search('psp');
 
-   //Find psp in URL
-   var envBegin = locStr.search('psp');
+         //Substring the URL starting 4 positions after psp (this should be where the environment starts
+         var tempLocStr = locStr.substring(envBegin + 4);
 
-   //Substring the URL starting 4 positions after psp (this should be where the environment starts
-   var tempLocStr = locStr.substring(envBegin+4);
+         //Split the URL by / and get everything to the left of the first / (this should be our entire environment name
+         var envString = tempLocStr.split('/')[0];
 
-   //Split the URL by / and get everything to the left of the first / (this should be our entire environment name
-   var envString = tempLocStr.split('/')[0];
+         //If multiple windows are open, they will have _ followed by a number appended to the environment, remove this
+         var finalEnv = envString.split('_')[0];
 
-   //If multiple windows are open, they will have _ followed by a number appended to the environment, remove this
-   var finalEnv   = envString.split('_')[0];
+         //If greeting doesn't exist in header then add it
+         if ($("#pthdr2greeting").length == 0) {
+            $("div#pthdr2container").append("<div id='pthdr2greeting'><span class='greeting'>&nbsp;</span></div>");
+         }
 
-   //If greeting doesn't exist in header then add it
-   if($("#pthdr2greeting").length == 0) {
-      $("div#pthdr2container").append("<div id='pthdr2greeting'><span class='greeting'>&nbsp;</span></div>");
-   }
+         //Select greeting in the header
+         var $greet = $('#pthdr2greeting .greeting');
 
-   //Select greeting in the header
-    var $greet   = $('#pthdr2greeting .greeting');
-
-   //Populate greeting in header with environment
-   if ($.trim($greet.text()) == '') {
-      $greet.text(finalEnv);
-   }
-			}
-
+         //Populate greeting in header with environment
+         if ($.trim($greet.text()) == '') {
+            $greet.text(finalEnv);
+         }
+      }
    });
 
    //
-   // PS GREETING END
+   // GREETING END
    //
 
-$("div#pthdr2container").append("<div id='pthdr2shortcuts'><span class='shortcuts'>Shortcuts</span></div>");
-var $shortcuts = $('#pthdr2container .shortcuts');
+   //
+   // SHORTCUTS BEGIN
+   //
+   chrome.storage.sync.get('shortcutsOption', function(r) {
+      var shortcutsvalue = r['shortcutsOption'];
 
-// Array of Menu/Component/Markets/Descriptions of Peoplesoft "menuItem" objects
-var menuItems = [];
-menuItems.add = function(menu, component, market, descr) {
-  var self = this;
-  self.push({
-    menu: menu,
-    component: component,
-    market: market,
-    descr: descr,
-    speedKey: "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".charCodeAt(self.length)
-  });
-  return self;
-}
+      if (shortcutsvalue == 'Yes') {
+         $("div#pthdr2container").append("<div id='pthdr2shortcuts'><span class='shortcuts'>Shortcuts</span></div>");
 
-//        .../ps[c|p] /site     /portal   /node     /c /menuname   .component  .market
-var urlRe = /(.*)\/(ps[cp])\/([^\/]*)\/([^\/]*)\/([^\/]*)\/c\/([^\/\.]*)\.([^\/\.]*)\.([^\?]*).*/gi;
-var urlResult = urlRe.exec(window.location.href);
+         var $shortcuts = $('#pthdr2container .shortcuts');
 
-// if not component URL, check for homepage URL
-if (!urlResult) {
-  //        .../ps[c|p] /site     /portal   /node     /h /?tab=TABNAME
-  urlRe = /(.*)\/(ps[cp])\/([^\/]*)\/([^\/]*)\/([^\/]*)\/h\/\?tab\=([^\/\.]*)/gi;
-  urlResult = urlRe.exec(window.location.href);
-}
+         // Array of Menu/Component/Markets/Descriptions of Peoplesoft "menuItem" objects
+         var menuItems = [];
 
-// if url format recognised
-if (urlResult) {
-  // get url parts
-  var baseURL = urlResult[1];
-  var servlet = urlResult[2];
-  var site = urlResult[3].replace(/\_\d+/ig,'');
-  var portal = urlResult[4];
-  var node = urlResult[5];
+         menuItems.add = function(menu, component, market, descr) {
+            var self = this;
+            self.push({
+               menu: menu,
+               component: component,
+               market: market,
+               descr: descr,
+               speedKey: "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".charCodeAt(self.length)
+            });
+            return self;
+         }
 
-  // Add Menu/Component/Markets/Description menuItems...
-  menuItems
-    .add('UTILITIES'                    , 'PEOPLECODE_TRACE'             , 'GBL', 'PeopleTools: Trace PeopleCode'     )
-    .add('UTILITIES'                    , 'TRACE_SQL'                    , 'GBL', 'PeopleTools: Trace SQL'            )
-    .add('UTILITIES'                    , 'URL_TABLE'                    , 'GBL', 'PeopleTools: URLs'                 )
-    .add('UTILITIES'                    , 'MESSAGE_CATALOG1'             , 'GBL', 'PeopleTools: Message Catalogue'    )
-    .add('PROCESS_SCHEDULER'            , 'PRCSDEFN'                     , 'GBL', 'Processes:   Process Definitions'  )
-    .add('PROCESSMONITOR'               , 'PROCESSMONITOR'               , 'GBL', 'Processes:   Process Monitor'      )
-    .add('MAINTAIN_SECURITY'            , 'USERMAINT'                    , 'GBL', 'Security:    User Profiles'        )
-    .add('MAINTAIN_SECURITY'            , 'ACCESS_CNTRL_LISTX'           , 'GBL', 'Security:    Permission Lists'     )
-    .add('MAINTAIN_SECURITY'            , 'ROLEMAINT'                    , 'GBL', 'Security:    Roles'                )
-    .add('MAINTAIN_SECURITY'            , 'PSTREEMGRACC'                 , 'GBL', 'Query:       Query Access Manager' )
-    .add('QUERY_MANAGER'                , 'QUERY_MANAGER'                , 'GBL', 'Query:       Query Manager'        )
-    .add('PORTAL_ADMIN'                 , 'PORTAL_OBJ_LIST'              , 'GBL', 'Portal:      Structure and Content')
-    .add('UOG_MIG_REQUEST'              , 'UM_MIGRATION_SRCH'            , 'GBL', 'Migration:   Migration Search'     )
-  ;
+         //        .../ps[c|p] /site     /portal   /node     /c /menuname   .component  .market
+         var urlRe = /(.*)\/(ps[cp])\/([^\/]*)\/([^\/]*)\/([^\/]*)\/c\/([^\/\.]*)\.([^\/\.]*)\.([^\?]*).*/gi;
+         var urlResult = urlRe.exec(window.location.href);
 
-  // build up menu
-  var ul = $('<ul class="pscs-ul">');
+         // if not component URL, check for homepage URL
+         if (!urlResult) {
+            //        .../ps[c|p] /site     /portal   /node     /h /?tab=TABNAME
+            urlRe = /(.*)\/(ps[cp])\/([^\/]*)\/([^\/]*)\/([^\/]*)\/h\/\?tab\=([^\/\.]*)/gi;
+            urlResult = urlRe.exec(window.location.href);
+         }
 
-  for (var i = 0; i < menuItems.length; i++) {
-    // reconstruct urls for defined compoents
-    var url = baseURL;
-    url += '/psp';                       // force portal servlet
-    url += '/' + site + '_newwin';       // force new window
-    url += '/' + portal;
-    url += '/' + node;
-    url += '/c';
-    url += '/' + menuItems[i].menu;
-    url += '.' + menuItems[i].component;
-    url += '.' + menuItems[i].market;
+         // if url format recognised
+         if (urlResult) {
+            // get url parts
+            var baseURL = urlResult[1];
+            var servlet = urlResult[2];
+            var site = urlResult[3].replace(/\_\d+/ig, '');
+            var portal = urlResult[4];
+            var node = urlResult[5];
 
-    var li = $('<li class="pscs-li"><a target="_blank" href="' + url + '">' + String.fromCharCode(menuItems[i].speedKey) + ' : ' + menuItems[i].descr + '</a></li>');
-    $(li).attr('speedKey', menuItems[i].speedKey);
-    $(ul).append(li);
-  }
+            // Add Menu/Component/Markets/Description menuItems...
+            menuItems
+               .add('UTILITIES', 'PEOPLECODE_TRACE', 'GBL', 'PeopleTools: Trace PeopleCode')
+               .add('UTILITIES', 'TRACE_SQL', 'GBL', 'PeopleTools: Trace SQL')
+               .add('UTILITIES', 'URL_TABLE', 'GBL', 'PeopleTools: URLs')
+               .add('UTILITIES', 'MESSAGE_CATALOG1', 'GBL', 'PeopleTools: Message Catalogue')
+               .add('PROCESS_SCHEDULER', 'PRCSDEFN', 'GBL', 'Processes:   Process Definitions')
+               .add('PROCESSMONITOR', 'PROCESSMONITOR', 'GBL', 'Processes:   Process Monitor')
+               .add('MAINTAIN_SECURITY', 'USERMAINT', 'GBL', 'Security:    User Profiles')
+               .add('MAINTAIN_SECURITY', 'ACCESS_CNTRL_LISTX', 'GBL', 'Security:    Permission Lists')
+               .add('MAINTAIN_SECURITY', 'ROLEMAINT', 'GBL', 'Security:    Roles')
+               .add('MAINTAIN_SECURITY', 'PSTREEMGRACC', 'GBL', 'Query:       Query Access Manager')
+               .add('QUERY_MANAGER', 'QUERY_MANAGER', 'GBL', 'Query:       Query Manager')
+               .add('PORTAL_ADMIN', 'PORTAL_OBJ_LIST', 'GBL', 'Portal:      Structure and Content')
+               .add('UOG_MIG_REQUEST', 'UM_MIGRATION_SRCH', 'GBL', 'Migration:   Migration Search')
+            ;
 
-  var sticky_div = $('<div>').append('<div class="pscs-title">Select Component</div>').append(ul);
-  sticky_div.hide().appendTo('head');
+            // build up menu
+            var ul = $('<ul class="pscs-ul">');
 
-  var sticky_options = {
-    'speed'       : 300,    // animations: fast, slow, or integer
-    'duplicates'  : false,  // true or false
-    'autoclose'   : 10000,  // integer or false
-    'imagePath'   : chrome.extension.getURL("images/close.png") // data encoded image URL (imagePath option provided by danjenkins/Sticky)
-  };
+            for (var i = 0; i < menuItems.length; i++) {
+               // reconstruct urls for defined compoents
+               var url = baseURL;
+               url += '/psp'; // force portal servlet
+               url += '/' + site + '_newwin'; // force new window
+               url += '/' + portal;
+               url += '/' + node;
+               url += '/c';
+               url += '/' + menuItems[i].menu;
+               url += '.' + menuItems[i].component;
+               url += '.' + menuItems[i].market;
+               var li = $('<li class="pscs-li"><a target="_blank" href="' + url + '">' + String.fromCharCode(menuItems[i].speedKey) + ' : ' + menuItems[i].descr + '</a></li>');
+               $(li).attr('speedKey', menuItems[i].speedKey);
+               $(ul).append(li);
+            }
 
-$(document).ready(function(){
-  $shortcuts.click(function(){
-    var s = $('.sticky:visible');
+            var sticky_div = $('<div>').append('<div class="pscs-title">Shortcuts</div>').append(ul);
+            sticky_div.hide().appendTo('head');
 
-      // hide if already visible
-      if ( $(s).size() ) {
-        $(s).hide();
+            var sticky_options = {
+               'speed': 300, // animations: fast, slow, or integer
+               'duplicates': false, // true or false
+               'autoclose': 100000, // integer or false
+               'imagePath': chrome.extension.getURL("images/close.png") // data encoded image URL (imagePath option provided by danjenkins/Sticky)
+            };
+
+            $(document).ready(function() {
+
+               $shortcuts.click(function() {
+                  var s = $('.sticky:visible');
+
+                  // hide if already visible
+                  if ($(s).size()) {
+                     $(s).hide();
+                  }
+                  else {
+                     $(sticky_div).sticky(false, sticky_options);
+                  }
+               });
+            });
+         }
       }
-      else {
-        $(sticky_div).sticky(false, sticky_options);
-      }
-  });
-});
-
-}
+   });
+   //
+   // SHORTCUTS END
+   //
 
 /* ===== Click on an element (borrowed from Facebook Fixer, @namespace http://userscripts.org/people/14536) ===== */
 function click(elm) {
-  var evt = document.createEvent('MouseEvents');
-  evt.initMouseEvent('click', true, true, window, 0, 1, 1, 1, 1, false, false, false, false, 0, null);
-  elm.dispatchEvent(evt);
+   var evt = document.createEvent('MouseEvents');
+   evt.initMouseEvent('click', true, true, window, 0, 1, 1, 1, 1, false, false, false, false, 0, null);
+   elm.dispatchEvent(evt);
 }
